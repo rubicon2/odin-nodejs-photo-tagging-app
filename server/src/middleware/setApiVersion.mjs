e8193b51edd95071ded269fd916393dbba019cc3
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-20 15:00:10 +0100

    Create middleware for setting API version on req for use in controller

---
+++ server/src/middleware/setApiVersion.mjs
@@ -0,0 +1,9 @@
+export default function setApiVersion(version) {
+  // Add api version to req, so can easily use that info in controller for specific logic.
+  // Not sure that is the best approach though - probably totally seperate controllers for each
+  // version is best. A v2 could re-export v1 except for changed methods?
+  return (req, res, next) => {
+    req.apiVersion = version;
+    next();
+  };
+}
