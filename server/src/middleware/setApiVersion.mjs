c81bea6ced2e42668c540d2fc719a74f5d601d99
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-26 12:13:28 +0100

    Test and improve setApiVersion middleware

--- server/src/middleware/setApiVersion.mjs
+++ server/src/middleware/setApiVersion.mjs
@@ -3,7 +3,10 @@ export default function setApiVersion(version) {
   // Not sure that is the best approach though - probably totally seperate controllers for each
   // version is best. A v2 could re-export v1 except for changed methods?
   return (req, res, next) => {
-    req.apiVersion = version;
+    if (typeof version !== 'object') {
+      req.apiVersion =
+        typeof version === 'function' ? version(req, res) : version;
+    }
     next();
   };
 }
