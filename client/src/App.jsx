5a717f08e3c011e8a64f41e866db15d7a1609c16
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-27 14:53:03 +0100

    Client: remove default vite and react rubbish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [msg, setMsg] = useState(null);
   const [image, setImage] = useState(null);
 
@@ -70,19 +67,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Photo Tagging App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <button onClick={hitServer}>Hit Dat Server</button>
         <button onClick={getPhoto}>Get Photo</button>
         <form onSubmit={postPhoto} encType="multipart/form-data">
