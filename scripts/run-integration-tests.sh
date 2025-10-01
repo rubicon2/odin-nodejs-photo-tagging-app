f430fe996163f27abcf9eef7473df53a482b9b7a
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-01 17:52:05 +0100

    Fix: integration tests not running in container
    Had some strange stuff going on with tests - then
    realised that the docker containers were being started,
    then vitest was being run on the host machine, not the
    containerized application.
    
    So I created a Dockerfile.dev which is used in the
    docker-compose file's server build. It merely changes
    the start up command to run vitest instead of npm run
    start - I tried to get this working with a command in
    the docker-compose.yml file, but it just didn't seem
    to work.
    
    Docker compose is also set up to watch and sync any
    changes in the local filesystem, so when a test or
    file changes, the file within the container is updated,
    vitest running in the container detects the change,
    and the tests re-run.

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,43 +1,15 @@
 #!/usr/bin/env bash
 
-# Double quotes necessary in case file paths have spaces or special characters.
-# This stops the special characters being interpreted as such by the shell.
-# Move into directory containing this file and then pwd to get absolute path.
-DIR="$(cd "$(dirname "${0}")" && pwd)"
-echo "Scripts located at "${DIR}""
+echo "Starting docker containers..."
 
-# Override value in .env so prisma migrate and client uses this instead.
-export SERVER_DATABASE_URL="postgres://postgres:postgres@localhost:5433/test"
+# Start test database and containerized backend for storing uploads.
+# Now backend is containerized, once it runs CMD the prisma migration will happen, then the tests will run.
+docker-compose up --build --watch
 
-echo "Test database url: ${SERVER_DATABASE_URL}"
+# Once script fails or tests are finished, destroy container and volumes.
+echo "Shutting down docker containers..."
 
-echo "Starting docker container..."
+# -v removes volumes.
+docker-compose down -v
 
-docker-compose up -d
-echo "Waiting for the database to be ready..."
-
-# Wait until database is up and running before trying to run prisma migration.
-# Need to do docker-compose exec which runs a command against containerized service.
-# The SELECT query only works once the db is running, even though "SELECT 1" does not actually hit the db.
-"${DIR}"/wait-until.sh "docker-compose exec -T db psql -d test -c 'SELECT 1'" 10
-
-if [ $? -eq 0 ]; then
-  echo "Database ready"
-  echo "Running prisma migrate dev..."
-  npx prisma migrate dev
-
-  # Check whether prisma migration was successful or not.
-  if [ $? -eq 0 ]; then
-    echo "Prisma migration finished"
-    echo "Running integration tests..."
-    # Script will be run from package scripts so root will be /odin-nodejs-photo-tagging-app. 
-    vitest -c ./vitest.int.config.js
-  else
-    echo "Prisma migration failed. Tests cancelled"
-  fi
-else
-  echo "Database timed out. Migration and tests cancelled"
-fi
-
-# Once script fails or tests are finished, destroy container.
-docker-compose down
+echo "Docker containers shut down"
