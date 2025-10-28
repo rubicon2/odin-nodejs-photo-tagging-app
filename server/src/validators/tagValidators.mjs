1d6f65d941ac83aea6c3bff445e666774e8ff335
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-28 14:51:07 +0000

    Implement validation chain for /photo/:id/tag POST request

---
+++ server/src/validators/tagValidators.mjs
@@ -0,0 +1,26 @@
+import { body } from 'express-validator';
+
+const createPostTagChain = () => [
+  body('posX')
+    .trim()
+    .notEmpty()
+    .withMessage('Position X is a required field')
+    .isFloat()
+    .withMessage('Position X should be a number')
+    .toFloat(),
+  body('posY')
+    .trim()
+    .notEmpty()
+    .withMessage('Position Y is a required field')
+    .isFloat()
+    .withMessage('Position Y should be a number')
+    .toFloat(),
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name is a required field')
+    .isAlphanumeric()
+    .withMessage('Name should contain alphanumeric characters only'),
+];
+
+export { createPostTagChain };
