01626abe23b8705e522c128527298d8ba3269964
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-28 16:12:59 +0000

    Simplify /api/v1 tests

--- tests/app.api.v1/app.api.v1.test.mjs
+++ tests/app.api.v1/app.api.v1.test.mjs
@@ -6,68 +6,50 @@ import request from 'supertest';
 
 describe('/api/v1/photo', () => {
   describe('GET', () => {
-    it('GET returns all db entries, with absolute urls and no tags', async () => {
+    it('responds with a status code 200 and all db entries, with absolute urls and no tags', async () => {
       // Setup environment, add data, etc.
       await postTestData();
       // Must return async request otherwise tests won't run properly! Will get incorrectly passing tests.
-      return request(app)
-        .get('/api/v1/photo')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-          expect(res.body).toStrictEqual({
-            status: 'success',
-            data: {
-              message: 'All photos successfully retrieved.',
-              photos: testImageDataAbsoluteUrl,
-            },
-          });
-        });
+      const response = await request(app).get('/api/v1/photo');
+
+      expect(response.statusCode).toStrictEqual(200);
+      expect(response.body).toStrictEqual({
+        status: 'success',
+        data: {
+          message: 'All photos successfully retrieved.',
+          photos: testImageDataAbsoluteUrl,
+        },
+      });
     });
   });
+});
 
-  describe('/:id', () => {
-    describe('GET', () => {
-      describe('with a valid id', () => {
-        it('responds with a status code 200', async () => {
-          await postTestData();
-          return request(app)
-            .get(`/api/v1/photo/${testImageDataAbsoluteUrl[0].id}`)
-            .expect(200);
-        });
+describe('/api/v1/photo/:id', () => {
+  describe('GET', () => {
+    it('with a valid id, responds with a status code 200 and db entry, with absolute url and no tags', async () => {
+      await postTestData();
+      const response = await request(app).get(
+        `/api/v1/photo/${testImageDataAbsoluteUrl[0].id}`,
+      );
 
-        it('returns db entry for matching photo id, with absolute url and no tags', async () => {
-          await postTestData();
-          return request(app)
-            .get(`/api/v1/photo/${testImageDataAbsoluteUrl[0].id}`)
-            .expect('Content-Type', /json/)
-            .then((res) => {
-              expect(res.body).toStrictEqual({
-                status: 'success',
-                data: {
-                  message: 'Photo successfully retrieved.',
-                  photo: testImageDataAbsoluteUrl[0],
-                },
-              });
-            });
-        });
+      expect(response.statusCode).toStrictEqual(200);
+      expect(response.body).toStrictEqual({
+        status: 'success',
+        data: {
+          message: 'Photo successfully retrieved.',
+          photo: testImageDataAbsoluteUrl[0],
+        },
       });
+    });
 
-      describe('with an invalid id', () => {
-        it('responds with a status code 404', () => {
-          return request(app).get('/api/v1/photo/my-made-up-id').expect(404);
-        });
-
-        it('responds with a json message', () => {
-          return request(app)
-            .get('/api/v1/photo/my-made-up-id')
-            .expect({
-              status: 'fail',
-              data: {
-                message: 'That photo does not exist.',
-              },
-            });
-        });
+    it('with an invalid id, responds with a status code 404 and a json message', async () => {
+      const response = await request(app).get('/api/v1/photo/my-made-up-id');
+      expect(response.statusCode).toStrictEqual(404);
+      expect(response.body).toStrictEqual({
+        status: 'fail',
+        data: {
+          message: 'That photo does not exist.',
+        },
       });
     });
   });
