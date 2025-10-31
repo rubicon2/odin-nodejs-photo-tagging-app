e037e2d23696f36d7a5d8e3c85975e963ad93820
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-31 16:59:37 +0000

    Create useAdminState hook for getting admin state of server

---
+++ client/src/hooks/useAdminState.jsx
@@ -0,0 +1,16 @@
+import * as api from '../ext/api.admin.mjs';
+import { useEffect, useState } from 'react';
+
+export default function useAdminState() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    api.fetchPhotosWithTags().then((res) => {
+      if (res.ok) {
+        setIsAdmin(true);
+      }
+    });
+  }, []);
+
+  return isAdmin;
+}
