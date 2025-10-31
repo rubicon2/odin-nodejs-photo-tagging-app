515df3e8bbfd1214a2cd71f5bbc1065f67373e18
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-31 14:41:04 +0000

    Add tests of dubious quality for getElementClickPos
    Testing the particular position an element is clicked
    on is actually very tricky. Testing library user events
    don't seem to provide the same event info as react,
    which makes testing with it difficult.
    
    Chose instead to create a fake event object with
    the properties I know the element uses. Not very good,
    since for example, if the react event API changes and some
    of these fields no longer exist, the test won't break even
    though this function would be broken in production.
    
    But if the events supplied by testing library user events
    don't match those supplied by react, those aren't much use
    either.

--- client/src/ext/getElementClickPos.mjs
+++ client/src/ext/getElementClickPos.mjs
@@ -5,6 +5,10 @@ export default function getElementClickPos(reactClickEvent) {
     );
   }
 
+  const { clientWidth, clientHeight } = reactClickEvent.target;
+  if (clientWidth === 0 || clientHeight === 0)
+    throw new Error('Element width or height is zero.');
+
   // Calculate as percentage of width and height to account for different screen sizes.
   // Use client width as it does not take into account borders or scroll bars.
   return {
