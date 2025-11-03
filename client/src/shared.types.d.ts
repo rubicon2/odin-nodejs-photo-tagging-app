5b0239c4bdf4502892fa6d9a245eab0ba91db316
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-03 16:31:40 +0000

    Create file to contain shared types to be used across the application

---
+++ client/src/shared.types.d.ts
@@ -0,0 +1,14 @@
+type Photo = {
+  readonly id: React.Key;
+  readonly altText: string;
+  readonly url: string;
+  readonly tags?: Array<Tag>;
+};
+
+type Tag = {
+  readonly id: React.Key;
+  readonly name: string;
+  readonly posX: Number;
+  readonly posY: Number;
+  readonly imageId: string;
+};
