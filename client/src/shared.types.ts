0d1b4daf4967b85acb0d42a930bf4b8694f05e5c
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-04 11:48:03 +0000

    Possibly fix bloody type not found errors?

--- client/src/shared.types.ts
+++ client/src/shared.types.ts
@@ -1,11 +1,11 @@
-type Photo = {
+export type Photo = {
   readonly id: React.Key;
   readonly altText: string;
   readonly url: string;
   readonly tags?: Array<Tag>;
 };
 
-type Tag = {
+export type Tag = {
   readonly id: React.Key;
   readonly name: string;
   readonly posX: Number;
