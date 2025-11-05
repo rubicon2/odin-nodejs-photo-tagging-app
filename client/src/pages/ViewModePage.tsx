96b89c29e4fba8d900f15e4d41a919f11566d49f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-05 15:04:15 +0000

    client: with new tagCount on user mode photos, display remaining tags to find and check if player has found all tags

--- client/src/pages/ViewModePage.tsx
+++ client/src/pages/ViewModePage.tsx
@@ -8,23 +8,32 @@ interface Props {
 }
 
 export default function ViewModePage({ onMessage = () => {} }: Props) {
-  const [photos, setPhotos] = useState<Array<Photo>>([]);
+  const [photos, setPhotos] = useState<Array<UserPhoto>>([]);
   const [selectedPhotoId, setSelectedPhotoId] = useState<React.Key | null>(
     null,
   );
   // Use an object to store tags, not a set. Set uses object equality so won't work for storing found tags.
   const [foundTags, setFoundTags] = useState<{ [key: string]: Tag }>({});
 
+  const photo = photos.find(({ id }) => id === selectedPhotoId);
+  const foundTagsCount = Object.keys(foundTags).length;
+
+  function checkIfAllTagsFound() {
+    if (photo && foundTagsCount === photo.tagCount) {
+      // Congratulate the player.
+      // Once player dismisses modal (or whatever it is), load a new image and clear foundTags.
+    }
+  }
+
   function onTagsFound(tags: Array<Tag>) {
     for (const tag of tags) {
       foundTags[tag.id.toString()] = tag;
     }
     setFoundTags(foundTags);
-    // Check if all tags are found. However photos do not include total number of tags yet.
-    // Need to change server to give tag count on non-admin route (may as well put on admin route too though).
+    checkIfAllTagsFound();
   }
 
-  function getRandomPhoto(photos: Array<Photo>) {
+  function getRandomPhoto(photos: Array<UserPhoto>) {
     const random = Math.floor(Math.random() * photos.length);
     return photos[random];
   }
@@ -62,8 +71,6 @@ export default function ViewModePage({ onMessage = () => {} }: Props) {
     }
   }, [photos]);
 
-  const photo = photos.find(({ id }) => id === selectedPhotoId);
-
   return (
     <>
       {photo && (
@@ -74,7 +81,9 @@ export default function ViewModePage({ onMessage = () => {} }: Props) {
             onNoTagsFound={() => console.log('No tags!')}
             onTagsFound={onTagsFound}
           />
-          <Overlay>My Overlaid Text!</Overlay>
+          <Overlay>
+            {foundTagsCount}/{photo.tagCount.toString()} found
+          </Overlay>
         </div>
       )}
     </>
