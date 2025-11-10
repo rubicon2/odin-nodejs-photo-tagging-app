ef07505237bd31df16907a5bb7867a5fbc5e738a
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-10 10:54:05 +0000

    client: create EditModePhotoTags component

---
+++ client/src/components/EditModePhotoTags.tsx
@@ -0,0 +1,79 @@
+interface Props {
+  tags: Array<EditableTag>;
+  onUpdate?: Function;
+  onDelete?: Function;
+  onSave?: React.FormEventHandler;
+}
+
+export default function EditPhotoTags({
+  tags,
+  onUpdate = () => {},
+  onDelete = () => {},
+  onSave = () => {},
+}: Props) {
+  return (
+    <div>
+      <h4>Edit Tags Panel</h4>
+      <form onSubmit={onSave}>
+        {tags.map((tag, index) => (
+          <fieldset key={index}>
+            <legend>{tag.id || 'New Tag'}</legend>
+            {/* Include id in form data. */}
+            <label style={{ display: 'none' }}>
+              Id:
+              <input
+                type="text"
+                name={`tags[${index}][id]`}
+                readOnly
+                value={tag.id as string}
+              />
+            </label>
+            <label>
+              Name:
+              <input
+                type="text"
+                name={`tags[${index}][name]`}
+                required
+                value={tag.name}
+                placeholder="Tag Name"
+                onChange={(e) =>
+                  onUpdate(index, { ...tag, name: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              X Position:
+              <input
+                type="number"
+                step="0.001"
+                name={`tags[${index}][posX]`}
+                required
+                value={tag.posX.toString()}
+                onChange={(e) =>
+                  onUpdate(index, { ...tag, posX: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Y Position:
+              <input
+                type="number"
+                step="0.001"
+                name={`tags[${index}][posY]`}
+                required
+                value={tag.posY.toString()}
+                onChange={(e) =>
+                  onUpdate(index, { ...tag, posY: e.target.value })
+                }
+              />
+            </label>
+            <button type="button" onClick={() => onDelete(index)}>
+              Delete
+            </button>
+          </fieldset>
+        ))}
+        <button type="submit">Save Tags</button>
+      </form>
+    </div>
+  );
+}
