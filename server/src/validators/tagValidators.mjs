5fa468e10068e7a6e72e2eaa680d48dd379d1d75
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-11 15:23:04 +0000

    server: tagValidators: create validation chains for /api/v1/admin/photo/:photoId/tag PUT route

--- server/src/validators/tagValidators.mjs
+++ server/src/validators/tagValidators.mjs
@@ -1,4 +1,22 @@
+import db from '../db/client.mjs';
 import { body } from 'express-validator';
+
+// This is for use when the tag id is part of the req.body, not part of the route params.
+const createTagIdChain = (field = 'id') =>
+  body(field)
+    .trim()
+    .notEmpty()
+    .withMessage(`${field} is a required field`)
+    .isString()
+    .withMessage(`${field} should be a string`)
+    .custom(async (value) => {
+      const tag = await db.imageTag.findUnique({
+        where: {
+          id: value,
+        },
+      });
+      if (!tag) throw new Error(`Tag with id: ${value} does not exist`);
+    });
 
 const createPosXChain = (field = 'posX') =>
   body(field)
@@ -38,10 +56,49 @@ const createPutTagValidationChain = () => [
   createNameChain().optional(),
 ];
 
+const createCreateTagsArrayValidationChain = () => [
+  createNameChain('create.*.name'),
+  createPosXChain('create.*.posX'),
+  createPosYChain('create.*.posY'),
+];
+
+const createUpdateTagsArrayValidationChain = () => [
+  createTagIdChain('update.*.id'),
+  createNameChain('update.*.name').optional(),
+  createPosXChain('update.*.posX').optional(),
+  createPosYChain('update.*.posY').optional(),
+];
+
+const createDeleteTagsArrayValidationChain = () => [
+  createTagIdChain('delete.*'),
+];
+
+const createPutTagsRouteValidationChain = () => [
+  body('create')
+    .isArray()
+    .withMessage('create field is not an array')
+    .optional(),
+  body('update')
+    .isArray()
+    .withMessage('update field is not an array')
+    .optional(),
+  body('delete')
+    .isArray()
+    .withMessage('delete field is not an array')
+    .optional(),
+  ...createCreateTagsArrayValidationChain(),
+  ...createUpdateTagsArrayValidationChain(),
+  ...createDeleteTagsArrayValidationChain(),
+];
+
 export {
   createPosXChain,
   createPosYChain,
   createNameChain,
   createPostTagValidationChain,
   createPutTagValidationChain,
+  createCreateTagsArrayValidationChain,
+  createUpdateTagsArrayValidationChain,
+  createDeleteTagsArrayValidationChain,
+  createPutTagsRouteValidationChain,
 };
