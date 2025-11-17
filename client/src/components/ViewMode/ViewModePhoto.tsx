590dc5829574e183f32ab13559d1b710953eb069
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-17 17:07:12 +0000

    client: ViewModePhoto: improve style and positioning of overlaid tags found counter

--- client/src/components/ViewMode/ViewModePhoto.tsx
+++ client/src/components/ViewMode/ViewModePhoto.tsx
@@ -56,15 +56,20 @@ export default function ViewModePhoto({
         onClick={checkClickPos}
       />
       <Overlay>
-        <span
+        <div
           style={{
+            padding: '1rem',
             color: 'white',
             fontSize: '2rem',
-            textShadow: '2px 2px 2px orange',
+            // Standard line height from index.css is 1.5, but looks like extra padding here.
+            lineHeight: '1',
+            textShadow:
+              // So it scales with fontSize, use em or ch.
+              '0.05ch 0.05ch orange, 0.1ch 0.1ch red, 0.2ch 0.2ch 5px black',
           }}
         >
           {foundTags.length}/{photo.tagCount} found
-        </span>
+        </div>
       </Overlay>
     </PhotoContainer>
   );
