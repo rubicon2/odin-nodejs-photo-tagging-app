fcfbeaadac9de24d183545e9e52a4054b8438229
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-17 13:18:30 +0000

    client: style TitleBar component

--- client/src/components/TitleBar.tsx
+++ client/src/components/TitleBar.tsx
@@ -1,4 +1,34 @@
+import PaddedContainer from '../styled/PaddedContainer';
+import useDarkThemeActive from '../hooks/useDarkThemeActive';
 import React from 'react';
+import styled from 'styled-components';
+
+// Could not get these to work as button background images,
+// although apparently this is supposed to work. Vite does not
+// import svg as a url like with png or jpg, and later replace
+// with the dist version's url once everything has been packaged up.
+// Instead, the svg data itself is imported. Sources (e.g. css tricks)
+// say this should work with background-image: url(), but firefox and
+// chrome give an error: invalid property value.
+import settingsIconDark from '../img/icons/settings_48_dark.svg';
+import settingsIconLight from '../img/icons/settings_48_light.svg';
+
+const Content = styled(PaddedContainer)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    margin: 0;
+  }
+`;
+
+const SettingsButton = styled.button`
+  display: grid;
+  place-items: center;
+  padding: 10px;
+  background-color: transparent;
+`;
 
 interface Props {
   isAdminEnabled?: Boolean;
@@ -9,12 +39,15 @@ export default function TitleBar({
   isAdminEnabled = false,
   onSettingsClick = () => {},
 }: Props) {
+  const isDarkThemeActive = useDarkThemeActive();
   return (
     <header>
-      <h1>Where&apos;s Waldo?</h1>
-      <button onClick={onSettingsClick}>
-        {isAdminEnabled ? 'Disable Admin' : 'Enable Admin'}
-      </button>
+      <Content>
+        <h1>Where&apos;s Waldo?</h1>
+        <SettingsButton onClick={onSettingsClick}>
+          <img src={isDarkThemeActive ? settingsIconDark : settingsIconLight} />
+        </SettingsButton>
+      </Content>
     </header>
   );
 }
