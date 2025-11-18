16c2a7f9e0cd5e3c6e816220c8fe63a59f939e56
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-18 15:56:54 +0000

    client: create DangerButton styled component (e.g. for destructive actions)

---
+++ client/src/styled/DangerButton.tsx
@@ -0,0 +1,8 @@
+import ImportantButton from './ImportantButton';
+import styled from 'styled-components';
+
+const DangerButton = styled(ImportantButton)`
+  background-color: #ff3333;
+`;
+
+export default DangerButton;
