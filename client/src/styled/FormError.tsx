2f2a551303ad18e0d701d454d56126b328e6c045
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-11-19 13:01:16 +0000

    client: FormError styled component: improve text contrast

--- client/src/styled/FormError.tsx
+++ client/src/styled/FormError.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 const FormError = styled.span`
-  color: #dd3333;
+  color: #dd2222;
+
+  @media (prefers-color-scheme: dark) {
+    color: #ff5555;
+  }
 `;
 
 export default FormError;
