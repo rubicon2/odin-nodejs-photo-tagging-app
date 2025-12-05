a1d499e3880cba4a4e4bc25add291503f26c5264
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-12-05 15:08:50 +0000

    client > shared.types: change types to match api output

--- client/src/shared.types.d.ts
+++ client/src/shared.types.d.ts
@@ -6,11 +6,18 @@ type Photo = {
 
 interface UserPhoto extends Photo {
   readonly tagCount: number;
+  readonly tags: Array<UserTag>;
 }
 
 interface AdminPhoto extends Photo {
   readonly tags: Array<Tag>;
 }
+
+// This is horrible, 3 different types of tags?
+type UserTag = {
+  readonly id: React.Key;
+  readonly name: string;
+};
 
 type Tag = {
   readonly id: React.Key;
