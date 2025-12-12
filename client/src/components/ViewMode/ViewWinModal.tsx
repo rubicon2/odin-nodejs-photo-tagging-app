39c9bba0146897555369213c29dca07b0147d2d9
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-12-12 17:18:00 +0000

    client > ViewWinModal: fetch best times and show them to user

--- client/src/components/ViewMode/ViewWinModal.tsx
+++ client/src/components/ViewMode/ViewWinModal.tsx
@@ -1,6 +1,10 @@
 import Modal from '../Modal';
+import TimesTable from '../TimesTable';
 import ViewWinTimeForm from './ViewWinTimeForm';
 import { ModalContent, ModalHeaderCentered } from '../../styled/Modal';
+import * as api from '../../ext/api';
+
+import { useState, useLayoutEffect } from 'react';
 import styled from 'styled-components';
 
 const CenteredDiv = styled.div`
@@ -20,14 +24,40 @@ export default function ViewWinModal({
   onButtonClick = () => {},
   onClose = () => {},
 }: Props) {
+  const [bestTimes, setBestTimes] = useState<Array<Time>>([]);
+  const [msg, setMsg] = useState<string | null>(null);
   const time2DP = Math.round(time / 10) / 100;
+
+  // Update times every time the modal is set to active.
+  useLayoutEffect(() => {
+    // Clear out any previous messages.
+    setMsg(null);
+
+    async function fetchData() {
+      try {
+        const response = await api.fetchBestPhotoTimes();
+        const json = await response?.json();
+        if (response.ok) {
+          const bestTimes = json.data.bestTimes;
+          if (bestTimes) setBestTimes(bestTimes);
+        }
+      } catch (error: any) {
+        setMsg(error.message);
+      }
+    }
+
+    fetchData();
+  }, [isActive]);
 
   return (
     <Modal isActive={isActive} onClose={onClose}>
       <ModalContent>
         <ModalHeaderCentered>You Won!</ModalHeaderCentered>
         <CenteredDiv>You took {time2DP} seconds.</CenteredDiv>
+        <ModalHeaderCentered as="h3">Best Times</ModalHeaderCentered>
+        <TimesTable times={bestTimes} />
         <ViewWinTimeForm onFormSubmit={onButtonClick} />
+        {msg && <span>{msg}</span>}
       </ModalContent>
     </Modal>
   );
